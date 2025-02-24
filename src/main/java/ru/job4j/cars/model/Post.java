d3e5d7d5bb4ff14c2681880c40cbf48624cd329f
Aleksandr Kobelskiy <kobel.san.sanich@gmail.com>
2025-02-24 12:10:52 +0600

    1. ToMany [#301848]

---
+++ src/main/java/ru/job4j/cars/model/Post.java
@@ -0,0 +1,33 @@
+package ru.job4j.cars.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "auto_post")
+@Getter
+@Setter
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(name = "created", nullable = false, updatable = false, columnDefinition = "TIMESTAMP DEFAULT NOW()")
+    private LocalDateTime created = LocalDateTime.now();
+
+    @ManyToOne
+    @JoinColumn(name = "auto_user_id", nullable = false)
+    private User user;
+
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    private List<PriceHistory> priceHistories = new ArrayList<>();
+}
---
+++ src/main/java/ru/job4j/cars/model/PriceHistory.java
@@ -0,0 +1,32 @@
+package ru.job4j.cars.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "price_history")
+@Getter
+@Setter
+
+public class PriceHistory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "before", nullable = false)
+    private Long before;
+
+    @Column(name = "after", nullable = false)
+    private Long after;
+
+    @Column(name = "created", nullable = false, updatable = false, columnDefinition = "TIMESTAMP DEFAULT NOW()")
+    private LocalDateTime created = LocalDateTime.now();
+
+    @ManyToOne
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+}
